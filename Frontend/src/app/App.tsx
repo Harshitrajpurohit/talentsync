c55af1eea4b41c247f33f4a92e018031c7d25800
Harshitrajpurohit <indianhars10@gmail.com>
2026-07-20 10:39:38 +0530

    feature:setup react frontend

---
+++ Frontend/src/app/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  const a = 10;
+
+  const user = {
+    id: 1,
+    name: 'Harshit',
+  };
+
+  return (
+    <>
+      {a}
+      {user.name}
+    </>
+  );
+}
+
+export default App;
